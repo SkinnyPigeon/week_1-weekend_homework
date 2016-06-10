e599007b740739652326ebe82c22fde867ca9329
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-10 11:23:33 +0100

    first few passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,7 +1,19 @@
+require 'pry-byebug'
+
+# binding.pry
+
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
 
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, transaction)
+  return pet_shop[:admin][:total_cash] += transaction
+end
+
+def sold(pet_shop)
+  return [:admin][:pets_sold]
 end
