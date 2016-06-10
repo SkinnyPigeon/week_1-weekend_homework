6cd557f0e1406fdca8d4f5c1ffb14efc6e5040a1
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-10 13:42:30 +0100

    deletion works

--- pet_shop.rb
+++ pet_shop.rb
@@ -37,14 +37,23 @@ def pets_by_breed(pet_shop, breed_name)
 end
 
 def find_pet_by_name(pet_shop, pet_name)
-
   for pet in pet_shop[:pets]
     return pet if pet[:name] == pet_name
   end 
   return nil
 end
 
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      pet.delete(:name) 
+    end
+  end
+  return nil
+end
 
+def add_pet_to_stock(pet_shop, pet_name)
+end
 
 
 
